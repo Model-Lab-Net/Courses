b8bd508ee8dd1aa56e029c9bc46ee324d3b3c793
Model-Lab <64527779+Model-Lab-Net@users.noreply.github.com>
2025-07-06 11:35:39 +0300

    Update Install_R.cmd

--- Epi/Install_R.cmd
+++ Epi/Install_R.cmd
@@ -14,13 +14,13 @@ set CURL_VERSION=8.14.1_2
 c:
 md c:\temp
 cd c:\temp
-if not exist curl.zip powershell Invoke-WebRequest -Uri 'https://curl.se/windows/dl-%CURL_VERSION%/curl-%CURL_VERSION%-win64-mingw.zip' -OutFile 'c:\temp\curl.zip'
-powershell Expand-Archive -Path "C:\temp\curl.zip" -DestinationPath "C:\Temp" 
-move c:\temp\curl-%CURL_VERSION%-win64-mingw\bin\*.* c:\temp
+if not exist c:\temp\curl.zip powershell Invoke-WebRequest -Uri 'https://curl.se/windows/dl-%CURL_VERSION%/curl-%CURL_VERSION%-win64-mingw.zip' -OutFile 'c:\temp\curl.zip'
+if not exist c:\temp\curl.exe powershell Expand-Archive -Path "C:\temp\curl.zip" -DestinationPath "C:\Temp" 
+move /Y c:\temp\curl-%CURL_VERSION%-win64-mingw\bin\*.* c:\temp
 
 
 
-echo -------------------- Download R and RStudio installers ---------------------
+::echo -------------------- Download R and RStudio installers ---------------------
 :R_RStudio_Installers
 ::curl -o r.exe https://cran.r-project.org/bin/windows/base/R-%R_VERSION%-win.exe  
 ::curl -o rstudio.exe https://download1.rstudio.org/electron/windows/RStudio-%RSTUDIO_VERSION%.exe  
@@ -30,9 +30,9 @@ echo -------------------- Download R and RStudio installers --------------------
 
 echo --------------------------     Download R     ---------------------------
 :Install_R
-c:\temp\curl.exe --progress-bar -o  c:\temp\r.exe https://cran.r-project.org/bin/windows/base/R-%R_VERSION%-win.exe
+if not exist c:\temp\r.exe c:\temp\curl.exe --progress-bar -o  c:\temp\r.exe https://cran.r-project.org/bin/windows/base/R-%R_VERSION%-win.exe
 ::powershell Invoke-WebRequest -Uri 'https://cran.r-project.org/bin/windows/base/R-%R_VERSION%-win.exe' -OutFile 'c:\temp\r.exe'
-start /wait r.exe /VERYSILENT /NORESTART /SP-
+if not exist "%programfiles%\R\R-%R_VERSION%\bin" start /wait c:\temp\r.exe /VERYSILENT /NORESTART /SP-
 copy /Y "%programfiles%\R\R-%R_VERSION%\bin\x64\Rblas.dll"      "%programfiles%\R\R-%R_VERSION%\library\stats\libs\x64"
 copy /Y "%programfiles%\R\R-%R_VERSION%\bin\x64\Rlapack.dll"    "%programfiles%\R\R-%R_VERSION%\library\stats\libs\x64"
 
@@ -42,54 +42,53 @@ echo ----------------- Download VSCode  ---  ZIP for portable ------------------
 :IInstallVSCode
 ::if not exist c:\RVSCode md c:\RVSCode
 ::cd c:\RVSCode
-if not exist rvscode.zip c:\temp\curl.exe --progress-bar -o rvscode.zip https://vscode.download.prss.microsoft.com/dbazure/download/stable/%VSCODE_VERSION%.zip
+if not exist c:\temp\rvscode.zip c:\temp\curl.exe --progress-bar -o rvscode.zip https://vscode.download.prss.microsoft.com/dbazure/download/stable/%VSCODE_VERSION%
 ::powershell Invoke-WebRequest -Uri 'https://vscode.download.prss.microsoft.com/dbazure/download/stable/%VSCODE_VERSION%' -OutFile 'c:\Temp\rvscode.zip'
-powershell Expand-Archive -Path "C:\temp\rvscode.zip" -DestinationPath "C:\RVSCode" 
+if not exist c:\rvscode\code.exe powershell Expand-Archive -Path "C:\temp\rvscode.zip" -DestinationPath "C:\RVSCode" 
 
 
 ::Make folders for main course files
 md c:\RVSCode\data\user-data\User\
 md c:\RVSCode\R
-md c:\RVSCode\data\Course\EpiCode
-md c:\RVSCode\data\Course\EpiData
+md c:\RVSCode\Course\EpiCode
+md c:\RVSCode\Course\EpiData
 
 ::copy R to VSCode main folder
-robocopy "%programfiles%\R" "c:\RVSCode\R" /S
+robocopy "%programfiles%\R" "c:\RVSCode\R" /E
+
 
 ::install R languaugeserver package
-c:\temp\curl.exe --progress-bar -o c:\Temp\languageserver.zip https://cran.r-project.org/bin/windows/contrib/4.6/languageserver_%RLANGSERVER_VERSION%.zip
+if not exist c:\Temp\languageserver.zip c:\temp\curl.exe --progress-bar -o c:\Temp\languageserver.zip https://cran.r-project.org/bin/windows/contrib/4.6/languageserver_%RLANGSERVER_VERSION%.zip
 ::powershell Invoke-WebRequest -Uri 'https://cran.r-project.org/bin/windows/contrib/4.6/languageserver_%RLANGSERVER_VERSION%.zip' -OutFile 'c:\Temp\languageserver.zip'
-
-c:\RVSCode\R\R-%R_VERSION%\bin\R.exe CMD INSTALL c:\Temp\languageserver.zip
 c:\RVSCode\R\R-%R_VERSION%\bin\R.exe CMD INSTALL c:\Temp\languageserver.zip
 
 
-::Set settings.json for R in VSCode
+::set settings.json for R in VSCode
 (
 echo {
-echo     "r.rpath.windows": "C:\\RVSCode\\R\\R-%R_VERSION%\\bin\\R.exe",
+echo     "r.rpath.windows": "C:\\RVSCode\\R\\R-!R_VERSION!\\bin\\R.exe",
 echo     "editor.dropIntoEditor.preferences": [],
 echo     "r.rterm.option": [
-echo         "--r-binary=C:\\RVSCode\\R\\R-%R_VERSION%\\bin\\R.exe", 
-echo         "--no-save", 
+echo         "--r-binary=C:\\RVSCode\\R\\R-!R_VERSION!\\bin\\R.exe",
+echo         "--no-save",
 echo         "--no-restore"
 echo     ],
-echo     "r.rterm.windows": "C:\\RVSCode\\R\\R-%R_VERSION%\\bin\\R.exe",
+echo     "r.rterm.windows": "C:\\RVSCode\\R\\R-!R_VERSION!\\bin\\R.exe",
 echo     "r.bracketedPaste": true,
 echo     "r.sessionWatcher": true,
-echo     "editor.wordSeparators": "`~!@#%$^&*()-=+[{]}\\|;:'\",<>/?",
+echo     "editor.wordSeparators": "`~!@#%%$^&*()-=+[{]}\\|;:'\"",<>/?",
 echo     "r.plot.useHttpgd": true,
 echo     "terminal.integrated.profiles.windows": {
 echo         "R": {
-echo             "path": "C:\\RVSCode\\R\\R-%R_VERSION%\\bin\\R.exe",
+echo             "path": "C:\\RVSCode\\R\\R-!R_VERSION!\\bin\\R.exe",
 echo             "args": [ "--no-save", "--no-restore" ],
 echo             "env": {
-echo                 "PATH": "C:\\RVSCode\\R\\R-%R_VERSION%\\bin"
+echo                 "PATH": "C:\\RVSCode\\R\\R-!R_VERSION!\\bin"
 echo             }
 echo         }
 echo     }
 echo }
-) > c:\RVSCode\data\user-data\User\settings.json
+) > "C:\RVSCode\data\user-data\User\settings.json"
 
 
 ::Add extensions to VSCode
@@ -99,11 +98,12 @@ call C:\RVSCode\bin\code.cmd --install-extension reditorsupport.r
 call C:\RVSCode\bin\code.cmd --install-extension rdebugger.r-debugger
 
 ::Download first script to initialize for course
-curl -o c:\RVSCode\Course\Initialize_R_for_Epi.Rmd https://github.com/Model-Lab-Net/Courses/blob/main/Epi/Initialize_R_for_Epi.Rmd
+curl --progress-bar -o c:\RVSCode\Course\Initialize_R_for_Epi.Rmd https://github.com/Model-Lab-Net/Courses/blob/main/Epi/Initialize_R_for_Epi.Rmd
 ::powershell Invoke-WebRequest -Uri 'https://github.com/Model-Lab-Net/Courses/blob/main/Epi/Initialize_R_for_Epi.Rmd' -OutFile 'c:\RVSCode\Course\Initialize_R.Rmd'
 
 ::Create shortcut link to Desktop
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%ALLUSERSPROFILE%\Desktop\RVSCode.lnk');$s.TargetPath='C:\RVSCode\code.exe';$s.IconLocation='C:\RVSCode\code.exe,0';$s.Save()"
+
 
 
 echo ---------------- Download RStudio  ---  ZIP for portable --------------------
@@ -195,6 +195,8 @@ if exist c:\temp\r.exe del /F /S c:\temp\r.exe
 if exist c:\temp\rstudio.zip del /F /S c:\temp\rstudio.zip
 if exist c:\tempCode\rvscode.zip   del /F /S c:\temp\rvscode.zip
 
+
+pause
 
 :exit
 timeout /T 15
