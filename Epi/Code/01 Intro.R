586fdaaeb768860fe759095b7494b5e7fcd6d4b8
Model-Lab <64527779+Model-Lab-Net@users.noreply.github.com>
2025-07-13 16:54:37 +0300

    Add files via upload

--- Epi/Code/01 Intro.R
+++ Epi/Code/01 Intro.R
@@ -1,6 +1,6 @@
 ###
-### name: דוד בורג
-### ID  : [מספר תעודת זהות]
+### name  : דוד בורג
+### ID    : [מספר תעודת זהות]
 ### course: אפידמיולוגיה (HIT.ac.il)
 ### lesson: מבוא 01   
 ### date  : 03/07/2025
@@ -8,6 +8,8 @@
 ###  נכיר כאן פעולות בסיסיות ממש
 
 
+## An element is assigned to an object by using “=“ or  “<-“
+## An object is 'called' just by typing it’s name:
 height = 172
 height 
 
@@ -16,7 +18,7 @@ weight
 bmi = 70/(height/100)^2
 bmi
 
-### Variable classes- Atomic classes
+### Variable classes
 ### • Numeric class (integer / double classes)
 ### • Character class (string)
 ### • Logical class (Boolean)
@@ -57,3 +59,95 @@ l1
 ### ---= Factors (nominal variables) =---
 cities = c(1,2,5,4,2,4,3,4,3,5)          # c() is the combine function. 
 cities
+## now label each city with their names
+Fcities=factor(cities,labels=c("Ashdod","Beer-Sheva","Tel-Aviv","Jerusalem","Haifa"))
+Fcities
+## now convert the factor to a character vector
+levels(Fcities) = c("first","second","third","fourth","fifth")
+Fcities
+levels(cities) = c("first","second","third","fourth","fifth")
+cities
+Fcities
+Fcities[3]
+
+### ---= Vectors (a list of numbers) =---
+## Numerical vector
+v1 <- c(1,2,3,4,5)     # this combines the numbers into a vector
+v1
+
+v2 <- 1:5              # this creates a vector with numbers from 1 to 5
+v2
+
+# Replicating vectors
+r1 = rep(1,15)           # this replicates the number 1 fifteen times
+r1
+
+r2 <- rep(c(1,2,3),4)   # this replicates the numbers 1,2,3 four times
+r2
+
+## Character vector
+c1 = c('a','b','c','d','e')
+c1
+
+# Boolean vector
+d1 = c(TRUE,FALSE,FALSE,FALSE,FALSE)     # this creates a vector with TRUE and FALSE values
+d1
+
+e1 = (r2==2)                             # this creates a boolean vector that checks if each element in v2 is equal to 2
+e1
+
+# generate a random normal distribution
+n2 <- rnorm(15, 3, 2)       # this generates 15 random numbers from a normal distribution with mean=3 and standard deviation=2
+n2
+
+### ---= Matrices (a table of numbers) =---
+mat1 <- matrix(1:9, nrow=3 , ncol=3)
+mat1
+# Nottice the row and column designation
+
+### ---= Dataframe (similar to matrices but stores any kind of variable) =---
+df1 <- data.frame("norm1" = rnorm(5,1,0.5), "rep1" = rep(1,5),
+               "city" = c("Ashdod","Beer-Sheva","Tel-Aviv","Jerusalem",
+                          "Haifa"))
+df1
+
+## Rename column titles
+colnames(df1) = c("normal","replication","cities")
+df1
+## Rename column titles
+rownames(df1) = c("one","two","three","four","five")
+df1
+
+## Retrieving a column (as a vector)
+df1[, 3]              # this retrieves the third column as a vector
+df1[, "cities"]       # this retrieves the "cities" column as a vector
+df1$cities            # this also retrieves the "cities" column as a vector (simpler)
+
+## Add a column to the end of the dataframe
+df1 = cbind(df1,"population"=c(4722,117500,43818,8252,27991))
+df1
+
+## Lists
+list1=list(v2,mat1,df1)    # create an output showing  vector v2,  matrix mat1, and dataframe df1
+list1
+names(l1)=c("vector","matrix","dataframe")    # give each one a name in the ouptput
+list1
+
+# Retrieve elements in a list of different classes
+l1[[3]]             # retrieves the third element in the list, which is the dataframe df1
+l1[[3]][, 3]        # retrieves the 4th column in df1       
+l1[[3]][4,]         # retrieves the 4th row in df1
+
+
+## ---= If statement (for logical loops) =---
+x <- 42
+y <- 50
+
+if (y>x){           # check if y is greater than x
+  print(TRUE)       # if true, print TRUE
+}
+
+# Ifelse statement (for vectorized logical loops)
+v3 <- c(12,34,56,1,4,45)                    # create a vector v3
+condition <- ifelse(v3 > 10 ,'yes','no')    # ifelse function checks each element in v3, if it's greater than 10, it returns 'yes', otherwise 'no'
+condition
