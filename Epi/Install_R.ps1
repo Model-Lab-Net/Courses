555ed89d7ce765699e31c9acf1931c99a802aa3b
Model-Lab <64527779+Model-Lab-Net@users.noreply.github.com>
2025-11-30 07:22:57 +0200

    Replace curl with wget for downloads in Install_R.ps1

--- Epi/Install_R.ps1
+++ Epi/Install_R.ps1
@@ -12,6 +12,7 @@ $RSTUDIO_VERSION = "2025.09.2-418"
 $VSCODE_VERSION = "bf9252a2fb45be6893dd8870c0bf37e2e1766d61/VSCode-win32-x64-1.106.3.zip"
 $RLANGSERVER_VERSION = "0.3.16"
 $CURL_VERSION = "8.17.0_4"
+$WGET_VERSION = "1.21.4"
 
 Set-Location -Path "C:\"
 if (-not (Test-Path -Path "C:\temp")) { New-Item -Path "C:\temp" -ItemType Directory }
@@ -21,8 +22,16 @@ if (-not (Test-Path -Path "C:\temp\curl.zip")) {
     Invoke-WebRequest -Uri "https://curl.se/windows/dl-$CURL_VERSION/curl-$CURL_VERSION-win64-mingw.zip" -OutFile "C:\temp\curl.zip"
 }
 if (-not (Test-Path -Path "C:\temp\curl.exe")) {
-    Expand-Archive -Path "C:\temp\curl.zip" -DestinationPath "C:\temp"
+    Expand-Archive -Path "C:\temp\curl.zip" -DestinationPath "C:\temp" -Force
     Move-Item -Path "C:\temp\curl-$CURL_VERSION-win64-mingw\bin\*.*" -Destination "C:\temp" -Force
+}
+
+if (-not (Test-Path -Path "C:\temp\wget.zip")) {
+    Invoke-WebRequest -Uri "https://eternallybored.org/misc/wget/releases/wget-$WGET_VERSION-win64.zip" -O "C:\temp\wget.zip"
+}
+if (-not (Test-Path -Path "C:\temp\wget.exe")) {
+    Expand-Archive -Path "C:\temp\wget.zip" -DestinationPath "C:\temp" -Force
+    #Move-Item -Path "C:\temp\curl-$CURL_VERSION-win64-mingw\bin\*.*" -Destination "C:\temp" -Force
 }
 
 if (-not (Test-Path -Path "C:\temp\7.zip")) {
@@ -35,7 +44,8 @@ if (-not (Test-Path -Path "C:\temp\7z.exe")) {
 # -------------------------- Download R ---------------------------
 Write-Output "Downloading R..."
 if (-not (Test-Path -Path "C:\temp\r.exe")) {
-    & "C:\temp\curl.exe" --progress-bar -o "C:\temp\r.exe" "https://cran.r-project.org/bin/windows/base/R-$R_VERSION-win.exe"
+    # & "C:\temp\curl.exe" --progress-bar -o "C:\temp\r.exe" "https://cran.r-project.org/bin/windows/base/R-$R_VERSION-win.exe"
+    & "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\temp\r.exe" "https://cran.r-project.org/bin/windows/base/R-$R_VERSION-win.exe"
 }
 if (-not (Test-Path -Path "c:\RVScode\R\bin")) {
     Start-Process -Verb RunAs -FilePath "C:\temp\r.exe" -ArgumentList "/SILENT", "/NORESTART", "/MERGETASKS=!desktopicon", "/SP-", "/DIR=`"c:\RVScode\R`"" -Wait
@@ -46,7 +56,8 @@ Copy-Item -Path "c:\RVScode\R\bin\x64\Rlapack.dll" -Destination "c:\RVScode\R\li
 # ----------------- Download VSCode --- ZIP for portable ------------------
 Write-Output "Downloading VSCode..."
 if (-not (Test-Path -Path "C:\temp\rvscode.zip")) {
-    & "C:\temp\curl.exe" --progress-bar -o "C:\temp\rvscode.zip" "https://vscode.download.prss.microsoft.com/dbazure/download/stable/$VSCODE_VERSION"
+    # & "C:\temp\curl.exe" --progress-bar -o "C:\temp\rvscode.zip" "https://vscode.download.prss.microsoft.com/dbazure/download/stable/$VSCODE_VERSION"
+    & "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\temp\rvscode.zip" "https://vscode.download.prss.microsoft.com/dbazure/download/stable/$VSCODE_VERSION"
 }
 if (-not (Test-Path -Path "C:\RVSCode\code.exe")) {
     New-Item -Path "C:\RVSCode" -ItemType Directory -Force
@@ -64,7 +75,8 @@ robocopy "$env:ProgramFiles\R" "C:\RVSCode\R" /E /NFL /NDL /NJH /NJS /MT:4
 
 # Install R languageserver package
 if (-not (Test-Path -Path "C:\temp\languageserver.zip")) {
-    & "C:\temp\curl.exe" --progress-bar -o "C:\temp\languageserver.zip" "https://cran.r-project.org/bin/windows/contrib/4.6/languageserver_$RLANGSERVER_VERSION.zip"
+    # & "C:\temp\curl.exe" --progress-bar -o "C:\temp\languageserver.zip" "https://cran.r-project.org/bin/windows/contrib/4.6/languageserver_$RLANGSERVER_VERSION.zip"
+    & "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\temp\languageserver.zip" "https://cran.r-project.org/bin/windows/contrib/4.6/languageserver_$RLANGSERVER_VERSION.zip"
 }
 & "C:\RVSCode\R\bin\R.exe" CMD INSTALL "C:\temp\languageserver.zip"
 
@@ -103,7 +115,8 @@ $settingsJson | Out-File -FilePath "C:\RVSCode\data\user-data\User\settings.json
 & "C:\RVSCode\bin\code.cmd" --install-extension rdebugger.r-debugger
 
 # Download first script to initialize for course
-& "C:\temp\curl.exe" --progress-bar -o "C:\RVSCode\Course\Initialize_R.Rmd" "https://raw.githubusercontent.com/Model-Lab-Net/Courses/refs/heads/main/Epi/!Initialize_R.Rmd"
+# & "C:\temp\curl.exe" --progress-bar -o "C:\RVSCode\Course\Initialize_R.Rmd" "https://raw.githubusercontent.com/Model-Lab-Net/Courses/refs/heads/main/Epi/!Initialize_R.Rmd"
+& "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\RVSCode\Course\Initialize_R.Rmd" "https://raw.githubusercontent.com/Model-Lab-Net/Courses/refs/heads/main/Epi/!Initialize_R.Rmd"
 
 # Create shortcut link to Desktop
 $shell = New-Object -ComObject WScript.Shell
@@ -120,7 +133,8 @@ $vs.Save()
 # ---------------- Download RStudio --- ZIP for portable --------------------
 Write-Output "Downloading RStudio..."
 if (-not (Test-Path -Path "C:\temp\rstudio.zip")) {
-    & "C:\temp\curl.exe" --progress-bar -o "C:\temp\rstudio.zip" "https://download1.rstudio.org/electron/windows/RStudio-$RSTUDIO_VERSION.zip"
+    # & "C:\temp\curl.exe" --progress-bar -o "C:\temp\rstudio.zip" "https://download1.rstudio.org/electron/windows/RStudio-$RSTUDIO_VERSION.zip"
+    & "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\temp\rstudio.zip" "https://download1.rstudio.org/electron/windows/RStudio-$RSTUDIO_VERSION.zip"
 }
 if (-not (Test-Path -Path "C:\RStudio\rstudio.exe")) {
     New-Item -Path "C:\RStudio" -ItemType Directory -Force
