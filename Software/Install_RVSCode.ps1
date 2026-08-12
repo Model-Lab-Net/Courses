4437dc5d5ea6d109cacaa34219572aa46e677b5c
Model-Lab <64527779+Model-Lab-Net@users.noreply.github.com>
2026-08-12 13:20:10 +0300

    Add files via upload

---
+++ Software/Install_RVSCode.ps1
@@ -0,0 +1,192 @@
+# Title: PowerShell script to install R, RStudio, and R in VSCode
+# Author: David Burg
+# For: R course
+# Date: 30/11/2025
+# Source: https://www.youtube.com/watch?v=k79H8EeR5Jo
+#         https://www.youtube.com/watch?v=rKPfssR66GM
+#         https://www.datanovia.com/learn/tools/r-in-vscode/recommended-vscode-configurations-for-r-programming.html
+
+# ---------------------- Get everything ready -------------------------------
+$R_VERSION = "4.6.1"
+$RSTUDIO_VERSION = "2026.07.1-147"
+$VSCODE_VERSION = "df53daabb18cd157bdb08c7f01c34df936cf12f4/VSCode-win32-x64-1.132.0.zip"
+$RLANGSERVER_VERSION = "0.3.18"
+$CURL_VERSION = "8.21.0_6"
+$WGET_VERSION = "1.21.4"
+
+Set-Location -Path "C:\"
+if (-not (Test-Path -Path "C:\temp")) { New-Item -Path "C:\temp" -ItemType Directory }
+Set-Location -Path "C:\temp"
+
+if (-not (Test-Path -Path "C:\temp\curl.zip")) {
+    Invoke-WebRequest -Uri "https://curl.se/windows/dl-$CURL_VERSION/curl-$CURL_VERSION-win64-mingw.zip" -OutFile "C:\temp\curl.zip"
+}
+
+if (-not (Test-Path -Path "C:\temp\curl.exe")) {
+    Expand-Archive -Path "C:\temp\curl.zip" -DestinationPath "C:\temp" -Force
+    Move-Item -Path "C:\temp\curl-$CURL_VERSION-win64-mingw\bin\*.*" -Destination "C:\temp" -Force
+}
+
+if (-not (Test-Path -Path "C:\temp\wget.zip")) {
+    Invoke-WebRequest -Uri "https://eternallybored.org/misc/wget/releases/wget-$WGET_VERSION-win64.zip" -O "C:\temp\wget.zip"
+}
+
+if (-not (Test-Path -Path "C:\temp\wget.exe")) {
+    Expand-Archive -Path "C:\temp\wget.zip" -DestinationPath "C:\temp" -Force
+    #Move-Item -Path "C:\temp\curl-$CURL_VERSION-win64-mingw\bin\*.*" -Destination "C:\temp" -Force
+}
+
+if (-not (Test-Path -Path "C:\temp\7.zip")) {
+    & "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\temp\7za.zip" "https://www.7-zip.org/a/7za920.zip"
+    & "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\temp\7zip.7z" "https://www.7-zip.org/a/7z2501-extra.7z"
+}
+
+if (-not (Test-Path -Path "C:\temp\7za.exe")) {
+    & Expand-Archive -Path "C:\temp\7za.zip" -DestinationPath "C:\temp\7za" -Force
+    & "C:\temp\7za\7za.exe" x "C:\temp\7zip.7z" -o"C:\Temp" -y -mmt=on
+}
+
+# -------------------------- Download R ---------------------------
+Write-Output "Downloading R..."
+if (-not (Test-Path -Path "C:\temp\r.exe")) {
+    # & "C:\temp\curl.exe" --progress-bar -o "C:\temp\r.exe" "https://cran.r-project.org/bin/windows/base/R-$R_VERSION-win.exe"
+    & "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\temp\r.exe" "https://cran.r-project.org/bin/windows/base/old/$R_VERSION/R-$R_VERSION-win.exe"
+}
+
+if (-not (Test-Path -Path "c:\RVSCode\R\bin")) {
+    Start-Process -Verb RunAs -FilePath "C:\temp\r.exe" -ArgumentList "/SILENT", "/NORESTART", "/MERGETASKS=!desktopicon", "/SP-", "/DIR=`"c:\RVSCode\R`"" -Wait
+}
+
+Copy-Item -Path "c:\RVSCode\R\bin\x64\Rblas.dll" -Destination "c:\RVSCode\R\library\stats\libs\x64" -Force
+Copy-Item -Path "c:\RVSCode\R\bin\x64\Rlapack.dll" -Destination "c:\RVSCode\R\library\stats\libs\x64" -Force
+
+& "C:\RVSCode\R\bin\R.exe" -s -e "install.packages('languageserver', repos='https://cloud.r-project.org')"
+
+# ----------------- Download VSCode --- ZIP for portable ------------------
+Write-Output "Downloading VSCode..."
+if (-not (Test-Path -Path "C:\temp\RVSCode.zip")) {
+    # & "C:\temp\curl.exe" --progress-bar -o "C:\temp\RVSCode.zip" "https://vscode.download.prss.microsoft.com/dbazure/download/stable/$VSCODE_VERSION"
+    & "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\temp\vscode.zip" "https://vscode.download.prss.microsoft.com/dbazure/download/stable/$VSCODE_VERSION"
+}
+
+if (-not (Test-Path -Path "C:\RVSCode\code.exe")) {
+    New-Item -Path "C:\RVSCode" -ItemType Directory -Force
+    # Expand-Archive -Path "C:\temp\vscode.zip" -DestinationPath "C:\RVSCode"
+    & "C:\temp\7za.exe" x "C:\temp\vscode.zip" -o"C:\RVSCode" -y -mmt=on
+}
+
+# Make folders for main course files
+New-Item -Path "C:\RVSCode\data\user-data\User" -ItemType Directory -Force
+New-Item -Path "C:\RVSCode\R" -ItemType Directory -Force
+New-Item -Path "C:\RVSCode\Course\EpiCode" -ItemType Directory -Force
+New-Item -Path "C:\RVSCode\Course\EpiData" -ItemType Directory -Force
+
+# Copy R to VSCode main folder
+#robocopy "$env:ProgramFiles\R" "C:\RVSCode\R" /E /NFL /NDL /NJH /NJS /MT:4
+
+# Set settings.json for R in VSCode
+$settingsJson = @"
+{
+    "r.rpath.windows": "C:\\RVSCode\\R\\bin\\R.exe",
+    "editor.dropIntoEditor.preferences": [],
+    "r.rterm.option": [
+        "--r-binary=C:\\RVSCode\\R\\bin\\R.exe",
+        "--no-save",
+        "--no-restore"
+    ],
+    "r.rterm.windows": "C:\\RVSCode\\R\\bin\\R.exe",
+    "r.bracketedPaste": true,
+    "r.sessionWatcher": true,
+    "editor.wordSeparators": "`~!@#%$^&*()-=+[{]}\\|;:'\",<>/?",
+    "r.plot.useHttpgd": true,
+    "terminal.integrated.profiles.windows": {
+        "R": {
+            "path": "C:\\RVSCode\\R\\bin\\R.exe",
+            "args": [ "--no-save", "--no-restore" ],
+            "env": {
+                "PATH": "C:\\RVSCode\\R\\bin"
+            }
+        }
+    }
+}
+"@
+$settingsJson | Out-File -FilePath "C:\RVSCode\data\user-data\User\settings.json" -Encoding UTF8
+
+# Add extensions to VSCode
+& "C:\RVSCode\bin\code.cmd" --install-extension github.copilot --force > $null 2>&1
+# & "C:\RVSCode\bin\code.exe" --install-extension github.copilot-chat
+& "C:\RVSCode\bin\code.cmd" --install-extension reditorsupport.r --force > $null 2>&1
+& "C:\RVSCode\bin\code.cmd" --install-extension rdebugger.r-debugger --force > $null 2>&1
+
+# Download first script to initialize for course
+# & "C:\temp\curl.exe" --progress-bar -o "C:\RVSCode\Course\Initialize_R.Rmd" "https://raw.githubusercontent.com/Model-Lab-Net/Courses/refs/heads/main/Epi/!Initialize_R_for_Epi.Rmd"
+& "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\RVSCode\Course\Initialize_R.Rmd" "https://raw.githubusercontent.com/Model-Lab-Net/Courses/refs/heads/main/Epi/!Initialize_R_for_Epi.Rmd"
+
+# Create shortcut link to Desktop
+$shell = New-Object -ComObject WScript.Shell
+# $desktop = [Environment]::GetFolderPath('Desktop')
+$desktop = 'C:\Users\Public\Desktop'
+# VSCode shortcut
+$vs = $shell.CreateShortcut("$desktop\RVSCode.lnk")
+$vs.TargetPath = "C:\RVSCode\code.exe"
+$vs.Arguments = '"C:\RVSCode\Course"'
+$vs.IconLocation = "C:\RVSCode\code.exe,0"
+$vs.WorkingDirectory = "C:\RVSCode"
+$vs.Save()
+
+# ---------------- Download RStudio --- ZIP for portable --------------------
+Write-Output "Downloading RStudio..."
+if (-not (Test-Path -Path "C:\temp\rstudio.zip")) {
+    # & "C:\temp\curl.exe" --progress-bar -o "C:\temp\rstudio.zip" "https://download1.rstudio.org/electron/windows/RStudio-$RSTUDIO_VERSION.zip"
+    & "C:\temp\wget.exe" --no-verbose --show-progress -O "C:\temp\rstudio.zip" "https://download1.rstudio.org/electron/windows/RStudio-$RSTUDIO_VERSION.zip"
+}
+if (-not (Test-Path -Path "C:\RStudio\rstudio.exe")) {
+    New-Item -Path "C:\RStudio" -ItemType Directory -Force
+    # Expand-Archive -Path "C:\temp\rstudio.zip" -DestinationPath "C:\RStudio"
+    & "C:\temp\7za.exe" x "C:\temp\rstudio.zip" -o"C:\RStudio" -y -mmt=on
+}
+
+# Download settings for RStudio
+# & "C:\temp\curl.exe" --progress-bar -o "C:\RStudio\user-data\rstudio-prefs.json" "https://drive.usercontent.google.com/download?id=19KaP4pbdM_O78gcgepxqE196SG0zE7fq"
+# & "C:\temp\curl.exe" --progress-bar -o "C:\RStudio\user-data\rstudio-desktop.json" "https://drive.usercontent.google.com/download?id=1priqCaKnSOOwCRU5J0anC8mH2gQMQYxE"
+
+# Make folders for RStudio
+New-Item -Path "C:\RStudio\Course" -ItemType Directory -Force
+New-Item -Path "C:\RStudio\user-data" -ItemType Directory -Force
+New-Item -Path "C:\RStudio\resources\stylesheets" -ItemType Directory -Force
+New-Item -Path "C:\RStudio\resources\themes" -ItemType Directory -Force
+
+
+# Copy R to RStudio main folder
+& robocopy "C:\RVSCode\R" "C:\RStudio\R" /E /NFL /NDL /NJH /NJS /MT:4
+
+# Set environment variables
+$env:RSTUDIO_WHICH_R = ".\R\bin\x64\R.exe"
+$env:RSTUDIO_CONFIG_HOME = "C:\RStudio\user-data"
+$env:RSTUDIO_DATA_HOME = "C:\RStudio\user-data"
+[Environment]::SetEnvironmentVariable("RSTUDIO_WHICH_R", $env:RSTUDIO_WHICH_R, "User")
+[Environment]::SetEnvironmentVariable("RSTUDIO_CONFIG_HOME", $env:RSTUDIO_CONFIG_HOME, "User")
+[Environment]::SetEnvironmentVariable("RSTUDIO_DATA_HOME", $env:RSTUDIO_DATA_HOME, "User")
+# [Environment]::SetEnvironmentVariable("RSTUDIO_WHICH_R", $env:RSTUDIO_WHICH_R, "Machine")
+#[Environment]::SetEnvironmentVariable("RSTUDIO_CONFIG_HOME", $env:RSTUDIO_CONFIG_HOME, "Machine")
+# [Environment]::SetEnvironmentVariable("RSTUDIO_DATA_HOME", $env:RSTUDIO_DATA_HOME, "Machine")
+
+# Create shortcut link on Desktop
+$shell = New-Object -ComObject WScript.Shell
+#$desktop = [Environment]::GetFolderPath('Desktop')
+$desktop = 'C:\Users\Public\Desktop'
+# RStudio shortcut
+$rs = $shell.CreateShortcut("$desktop\RStudio.lnk")
+$rs.TargetPath = "C:\RStudio\rstudio.exe"
+$rs.IconLocation = "C:\RStudio\rstudio.exe,0"
+$rs.WorkingDirectory = "C:\RStudio"
+$rs.Save()
+
+# -------------------- Uninstall R and cleanup ---------------------------
+Write-Output "Cleaning up..."
+# if (Test-Path -Path "C:\temp\r.exe") { Remove-Item -Path "C:\temp\r.exe" -Force }
+# if (Test-Path -Path "C:\temp\rstudio.zip") { Remove-Item -Path "C:\temp\rstudio.zip" -Force }
+# if (Test-Path -Path "C:\temp\RVSCode.zip") { Remove-Item -Path "C:\temp\RVSCode.zip" -Force }
+
+# Pause for 15 seconds before exiting
+Start-Sleep -Seconds 15
